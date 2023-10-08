1ab8c9559a50a2c805c363b9111b10ba6d6e62ac
AhmedRamy2000 <125711699+AhmedRamy2000@users.noreply.github.com>
2023-10-09 00:57:52 +0300

    finish onboarding

--- lib/views/welcome/welcome.dart
+++ lib/views/welcome/welcome.dart
@@ -2,6 +2,7 @@ import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:udemy/views/home.dart';
 import 'package:udemy/views/welcome/bloc/bloc/welcome_bloc.dart';
 
 class Welcome extends StatefulWidget {
@@ -12,6 +13,7 @@ class Welcome extends StatefulWidget {
 }
 
 class _WelcomeState extends State<Welcome> {
+  PageController pageController = PageController(initialPage: 0);
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -26,40 +28,39 @@ class _WelcomeState extends State<Welcome> {
                     alignment: Alignment.topCenter,
                     children: [
                       PageView(
-                        onPageChanged: (index){
-                          state.page =index;
-                          BlocProvider.of<WelcomeBloc>(context).add(WelcomeEvent());
-                          debugPrint('page number $index');
-                        },
-                        children: [
-                        OnBording(
-                          index: 0,
-                          image: 'assets/images/boy.png',
-                          context: context,
-                          buttonName: 'Next',
-                          title: 'First See Learning',
-                          subTitle:
+                          controller: pageController,
+                          onPageChanged: (index) {
+                            state.page = index;
+                            BlocProvider.of<WelcomeBloc>(context)
+                                .add(WelcomeEvent());
+                            debugPrint('page number $index');
+                          },
+                          children: [
+                            _page(
+                              1,
+                              context,
+                              'assets/images/boy.png',
+                              'Next',
+                              'First See Learning',
                               'Forget about a for of paper all knowledge in one learning',
-                        ),
-                        OnBording(
-                          index: 1,
-                          image: 'assets/images/man.png',
-                          context: context,
-                          buttonName: 'Next',
-                          title: 'Connect With Everyone',
-                          subTitle:
+                            ),
+                            _page(
+                              2,
+                              context,
+                              'assets/images/reading.png',
+                              'Next',
+                              'Connect With Everyone',
                               'Always keep in touch with your tutor & friend. let\'s get connected!',
-                        ),
-                        OnBording(
-                          index: 2,
-                          image: 'assets/images/reading.png',
-                          context: context,
-                          buttonName: 'Get Started',
-                          title: 'Always Fascinated Learning',
-                          subTitle:
+                            ),
+                            _page(
+                              3,
+                              context,
+                              'assets/images/man.png',
+                              'Get Started',
+                              'Always Fascinated Learning',
                               'Anywhere, anytime. the time is at your descretion so study whenever you want.',
-                        ),
-                      ]),
+                            ),
+                          ]),
                       Positioned(
                           bottom: 50.h,
                           child: DotsIndicator(
@@ -70,7 +71,7 @@ class _WelcomeState extends State<Welcome> {
                               color: Colors.grey,
                               activeColor: Colors.blue,
                               size: const Size.square(8.0),
-                              activeSize: const Size(18, 8),
+                              activeSize: const Size(20, 8),
                               activeShape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(5.0)),
                             ),
@@ -81,66 +82,71 @@ class _WelcomeState extends State<Welcome> {
           ),
         ));
   }
-}
 
-class OnBording extends StatelessWidget {
-  final int index;
-  final BuildContext context;
-  final String image;
-  final String buttonName;
-  final String title;
-  final String subTitle;
-  const OnBording({
-    super.key,
-    required this.index,
-    required this.context,
-    required this.image,
-    required this.title,
-    required this.subTitle,
-    required this.buttonName,
-  });
-
-  @override
-  Widget build(BuildContext context) {
+  Widget _page(
+    int index,
+    BuildContext context,
+    String image,
+    String buttonName,
+    String title,
+    String subTitle,
+  ) {
     return Column(
       children: [
         SizedBox(
           width: 345.w,
           height: 345.w,
-          child: Image.asset(image,fit:BoxFit.cover),
+          child: Image.asset(image, fit: BoxFit.cover),
         ),
         Text(title,
             style: TextStyle(
                 color: Colors.black,
                 fontSize: 24.sp,
                 fontWeight: FontWeight.normal)),
-        Text(subTitle,
-            style: TextStyle(
-                color: Colors.black.withOpacity(0.5),
-                fontSize: 14.sp,
-                fontWeight: FontWeight.normal)),
         Container(
-            margin: EdgeInsets.only(top: 100.h, left: 25.w, right: 25.w),
-            width: 325.w,
-            height: 50.w,
-            decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 88, 174, 245),
-                borderRadius: BorderRadius.all(Radius.circular(15.w)),
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.grey.withOpacity(0.1),
-                      spreadRadius: 1,
-                      blurRadius: 2,
-                      offset: const Offset(5, 10))
-                ]),
-            child: Center(
-                child: Text(
-              buttonName,
+          width: 375.w,
+          padding: EdgeInsets.only(left: 30.w, right: 30.w),
+          child: Text(subTitle,
               style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16.sp,
-                  fontWeight: FontWeight.normal),
-            )))
+                  color: Colors.black.withOpacity(0.5),
+                  fontSize: 14.sp,
+                  fontWeight: FontWeight.normal)),
+        ),
+        GestureDetector(
+          onTap: () {
+            if (index < 3) {
+              //do animation
+              pageController.animateToPage(index,
+                  duration: const Duration(microseconds: 500),
+                  curve: Curves.ease);
+            } else {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => const MyHome()));
+            }
+          },
+          child: Container(
+              margin: EdgeInsets.only(top: 100.h, left: 25.w, right: 25.w),
+              width: 325.w,
+              height: 50.w,
+              decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 88, 174, 245),
+                  borderRadius: BorderRadius.all(Radius.circular(15.w)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.withOpacity(0.1),
+                        spreadRadius: 1,
+                        blurRadius: 2,
+                        offset: const Offset(5, 10))
+                  ]),
+              child: Center(
+                  child: Text(
+                buttonName,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.normal),
+              ))),
+        )
       ],
     );
   }
